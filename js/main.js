60e8ce46cd73a213865d4295bcd35fe25489e282
abdulhamiid <adioabdulhamid@yahoo.com>
2022-05-18 08:56:56 +0100

    Adjust elements spacing in the stylesheet

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const menu = document.querySelector('.menu');
 const navList = document.querySelector('.nav-list');
-const closeMenu = document.querySelector('#close')
+const closeMenu = document.querySelector('#close');
 menu.addEventListener('click', () => {
   navList.classList.toggle('hide');
   document.body.classList.toggle('no-scroll');
-})
+});
 
 closeMenu.addEventListener('click', () => {
   navList.classList.toggle('hide');
   document.body.classList.toggle('no-scroll');
-})
+});
